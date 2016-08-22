393a3049b670ece66438f06e059e54fa58eecc8e
ucheol2 <cokr3430@gmail.com>
2016-08-22 21:51:36 +0900

    master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,13 @@ class HomeController < ApplicationController
   end
 
   def game
+    @left = (1..90).to_a.sample
+    @top = (1..90).to_a.sample
+    @score = params[:score].to_i
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
