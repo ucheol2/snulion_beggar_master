b39b4e238f84c19bf7fc59229a48075d3ebe52e8
ucheol2 <cokr3430@gmail.com>
2016-08-22 20:14:33 +0900

    index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
+  get 'game' => 'home#game', as: :game
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
